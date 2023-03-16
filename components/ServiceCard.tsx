e42b0a7d201f6bb7158303b406b2702de7761183
Scott <scott@scotts-mbp.lan>
2023-03-16 08:23:21 -0700

    fixing

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -3,9 +3,12 @@ import React from "react";
 
 export default function ServiceCard({ service }: Service) {
   return (
-    <div className="bg-gray-100 border border-gray-200 p-6 rounded-lg">
-      <h3 className="text-lg font-medium text-gray-900">{service.title}</h3>
-      <p className="mt-4 text-base text-gray-600">{service.desc}</p>
-    </div>
+    <>
+      <pre>{JSON.stringify(service, null, 2)}</pre>
+      {/* <div className="bg-gray-100 border border-gray-200 p-6 rounded-lg">
+        <h3 className="text-lg font-medium text-gray-900">{service.title}</h3>
+        <p className="mt-4 text-base text-gray-600">{service.desc}</p>
+      </div> */}
+    </>
   );
 }
