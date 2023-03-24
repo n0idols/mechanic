eb0a57031ab7372576e9293a9390175318021293
Scott <scott@scotts-mbp.lan>
2023-03-23 17:06:58 -0700

    account page fix

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,18 @@
-import Dashboard from "@/components/auth/Dashboard";
-import React from "react";
+import LoginBtn from "@/components/LoginBtn";
+import Paper from "@/components/Paper";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
-export default function page() {
+export default async function page() {
+  const session = await getServerSession(authOptions);
+
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <Paper>
+      <LoginBtn />
+      {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+      <h1>Welcome, {session?.user?.name}! 👋</h1>
+
+      <h1>My Appointments</h1>
+    </Paper>
   );
 }
